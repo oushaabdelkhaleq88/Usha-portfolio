57824ffc99ba7cc9834812c05831dde12e9789dc
lucidreamer <ashrafabdelkhaleqali@gmail.com>
2025-08-11 11:43:48 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -138,12 +138,16 @@ function AboutSection() {
             order: 1
           }} className="lg:order-2 lg:max-w-none lg:w-96 lg:h-96">
             <img 
-              src={logo} 
+              src={process.env.PUBLIC_URL + "/logo.png"} 
               alt="Ashraf Abdelkaleq"
               style={{
                 width: "100%",
                 height: "100%",
                 objectFit: "cover"
+              }}
+              onError={(e) => {
+                console.error("Image failed to load:", e.target.src);
+                e.target.style.display = "none";
               }}
             />
           </div>
